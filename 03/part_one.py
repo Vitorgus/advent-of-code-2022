42f5fba1f1bbe368a58d37fcbe3a1f80eb02d417
Vitor Augusto Pinheiro <26413854+Vitorgus@users.noreply.github.com>
2022-12-03 13:47:27 -0300

    challenge 03

---
+++ 03/part_one.py
@@ -0,0 +1,36 @@
+import os
+
+def get_filepath(file):
+  absolute_path = os.path.dirname(__file__)
+  return os.path.join(absolute_path, file)
+
+def get_priority(item):
+  if item.islower():
+    return ord(item) - ord('a') + 1
+  else:
+    return ord(item) - ord('A') + 27
+
+priority_sum = 0
+
+with open(get_filepath("input.txt"), encoding="utf-8") as f:
+  for line in f:
+    middle = len(line.strip()) // 2
+
+    first_compartiment = line[:middle]
+    second_compartiment = line[middle:]
+
+    duplicate_item = ''
+    found = False
+
+    for i in range(len(first_compartiment)):
+      for j in range(len(second_compartiment)):
+        if first_compartiment[i] == second_compartiment[j]:
+          duplicate_item = first_compartiment[i]
+          found = True
+          break
+      if found:
+        break
+    
+    priority_sum += get_priority(duplicate_item)
+
+print(priority_sum)
---
+++ 03/part_two.py
@@ -0,0 +1,37 @@
+import os
+
+def get_filepath(file):
+  absolute_path = os.path.dirname(__file__)
+  return os.path.join(absolute_path, file)
+
+def get_priority(item):
+  if item.islower():
+    return ord(item) - ord('a') + 1
+  else:
+    return ord(item) - ord('A') + 27
+
+GROUP_SIZE = 3
+
+elf_group = []
+priority_sum = 0
+
+with open(get_filepath("input.txt"), encoding="utf-8") as f:
+  for line in f:
+    elf_group.append(line.strip())
+
+    if len(elf_group) >= GROUP_SIZE:
+      for item in elf_group[0]:
+        all_elves_have_item = True
+
+        for elf in elf_group[1:]:
+          if not item in elf:
+            all_elves_have_item = False
+            break
+        
+        if all_elves_have_item:
+          priority_sum += get_priority(item)
+          break
+      
+      elf_group = []
+
+print(priority_sum)
