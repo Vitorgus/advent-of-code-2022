36edcb053b0805578f1b057a3bc21477d0288b9d
Vitor Augusto Pinheiro <26413854+Vitorgus@users.noreply.github.com>
2022-12-06 18:19:46 -0300

    challenge 05: part one

---
+++ 05/part_one.py
@@ -0,0 +1,45 @@
+import os
+import re
+
+def get_filepath(file):
+  absolute_path = os.path.dirname(__file__)
+  return os.path.join(absolute_path, file)
+
+stacks = []
+
+with open(get_filepath("input.txt"), encoding="utf-8") as f:
+  build_stacks = True
+  numbers_pattern = re.compile(r'\d+')
+
+  for line in f:
+    if line == '\n':
+      # Switch from building stacks to moving crates
+      build_stacks = False
+    elif build_stacks:
+      # Build stacks step
+      n_stacks = len(line) // 4
+
+      if len(stacks) == 0:
+        stacks = [[] for x in range(n_stacks)]
+      
+      for i in range(n_stacks):
+        item_location = i*4 + 1
+        item = line[item_location]
+
+        if item.isalpha():
+          stacks[i].insert(0, line[item_location])
+    else:
+      # Moving step
+      numbers = numbers_pattern.findall(line)
+
+      move_quantity = int(numbers[0])
+      stack_from = int(numbers[1]) - 1
+      stack_to = int(numbers[2]) - 1
+
+      for i in range(move_quantity):
+        stacks[stack_to].append(stacks[stack_from].pop())
+
+for i in range(len(stacks)):
+  print(stacks[i][-1], end="")
+
+print()
