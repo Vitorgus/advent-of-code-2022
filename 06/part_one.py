84f91d7a12c3efcde9fcc085926e9e5eb5fb2b91
Vitor Augusto Pinheiro <26413854+Vitorgus@users.noreply.github.com>
2022-12-07 16:56:39 -0300

    challenge 06

---
+++ 06/part_one.py
@@ -0,0 +1,17 @@
+import os
+
+def get_filepath(file):
+  absolute_path = os.path.dirname(__file__)
+  return os.path.join(absolute_path, file)
+
+MARKER_SIZE = 4
+
+with open(get_filepath("input.txt"), encoding="utf-8") as f:
+  line = f.readline()
+  
+  for i in range(len(line) - MARKER_SIZE):
+    marker_set = set(line[i:i + MARKER_SIZE])
+
+    if len(marker_set) == MARKER_SIZE:
+      print(i + MARKER_SIZE)
+      break
