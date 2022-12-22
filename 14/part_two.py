71a332a9f2cb554b5a1c9d42a9a7992e5a5411f5
Vitor Augusto Pinheiro <26413854+Vitorgus@users.noreply.github.com>
2022-12-22 19:54:03 -0300

    challenge 14: removed cave size limit by it's margins on part two

--- 14/part_two.py
+++ 14/part_two.py
@@ -11,49 +11,29 @@ class Cave:
   SAND_SOURCE_SYMBOL = '+'
   SAND_PATH_SYMBOL = '~'
 
-  MARGIN_LEFT = 150
-  MARGIN_RIGHT = 150
+  MARGIN_LEFT = 2
+  MARGIN_RIGHT = 2
 
   FLOOR_OFFSET = 2
 
   def __init__(self, rock_paths):
-    self._rock_paths = rock_paths
-
-    min_x = None
-    max_x = None
     max_y = None
 
     for path in rock_paths:
       for coord in path:
         coord_x, coord_y = coord
-        if min_x == None or coord_x < min_x:
-          min_x = coord_x
-        if max_x == None or coord_x > max_x:
-          max_x = coord_x
         if max_y == None or coord_y > max_y:
           max_y = coord_y
     
-    offset = max_x - min_x
     
-    self._min_x = min_x
-    self._max_x = max_x
-    self._max_y = max_y
-    self._offset = offset
+    self._floor_level = max_y + Cave.FLOOR_OFFSET
 
-    cave_width = offset + Cave.MARGIN_LEFT + Cave.MARGIN_RIGHT
-    cave_height = max_y + Cave.FLOOR_OFFSET
-
-    self._coords = [[Cave.AIR_SYMBOL for x in range(cave_width+1)] for y in range(cave_height+1)]
+    self._coords = {}
     self._last_sand_path = []
 
-    for path in self._rock_paths:
+    for path in rock_paths:
       for i in range(len(path) - 1):
         self._set_path(path[i], path[i+1])
-    
-    floor_y = max_y + Cave.FLOOR_OFFSET
-    floor_start = (min_x - Cave.MARGIN_LEFT, floor_y)
-    floor_end = (max_x + Cave.MARGIN_RIGHT, floor_y)
-    self._set_path(floor_start, floor_end)
     
     self._set_space(Cave.SAND_SOURCE_COORD, Cave.SAND_SOURCE_SYMBOL)
   
@@ -81,8 +61,6 @@ class Cave:
     current_x, current_y = current_coord
     
     next_y = current_y + 1
-    if next_y >= len(self._coords):
-      return None
     
     next_space = (current_x, next_y)
     if self._get_space(next_space) == Cave.AIR_SYMBOL:
@@ -120,30 +98,45 @@ class Cave:
         self._set_space((i, coord_y), Cave.ROCK_SYMBOL)
     else:
       raise Exception('Invalid path coordinates: {start} and {end} don\'t form a line')
-
-  def _translate_coords(self, coord):
-    coord_x, coord_y = coord
-
-    return (coord_x - self._min_x + Cave.MARGIN_LEFT, coord_y)
   
   def _set_space(self, coord, tile):
-    coord_x, coord_y = self._translate_coords(coord)
-    self._coords[coord_y][coord_x] = tile
+    self._coords[coord] = tile
   
   def _get_space(self, coord):
-    coord_x, coord_y = self._translate_coords(coord)
-    return self._coords[coord_y][coord_x]
+    coord_x, coord_y = coord
+    if coord_y >= self._floor_level:
+      return Cave.ROCK_SYMBOL
+
+    if coord not in self._coords:
+      return Cave.AIR_SYMBOL
+    
+    return self._coords[coord]
     
   def get_cave_string(self, last_sand_path=False):
     result = ''
-    sand_path = [self._translate_coords(coord) for coord in self._last_sand_path]
+
+    min_x = None
+    max_x = None
+
+    for coord in list(self._coords):
+      coord_x, coord_y = coord
+      if min_x == None or coord_x < min_x:
+        min_x = coord_x
+      if max_x == None or coord_x > max_x:
+        max_x = coord_x
     
-    for line in range(len(self._coords)):
-      for column in range(len(self._coords[line])):
-        if last_sand_path and (column, line) in sand_path:
+    min_x -= Cave.MARGIN_LEFT
+    max_x += Cave.MARGIN_RIGHT
+
+    sand_path = self._last_sand_path
+    
+    for coord_y in range(self._floor_level+1):
+      for coord_x in range(min_x, max_x+1):
+        coord = (coord_x, coord_y)
+        if last_sand_path and coord in sand_path:
           result += Cave.SAND_PATH_SYMBOL
         else:
-          result += self._coords[line][column]
+          result += self._get_space(coord)
       result += '\n'
     
     return result
@@ -172,7 +165,7 @@ with open(get_filepath("input.txt"), encoding="utf-8") as f:
     paths.append(path)
 
 DEBUG_PRINT_EACH_STEP = False
-DEBUG_PRINT_FINAL_STEP = False
+DEBUG_PRINT_FINAL_STEP = True
 
 cave = Cave(paths)
 
@@ -185,7 +178,8 @@ while cave.fall_sand() == True:
     print(cave)
 
 if DEBUG_PRINT_FINAL_STEP:
-  print('== FINAL STATE ==')
+  if DEBUG_PRINT_EACH_STEP:
+    print('== FINAL STATE ==')
   print(cave.get_cave_string(DEBUG_PRINT_FINAL_STEP))
 
 if DEBUG_PRINT_EACH_STEP or DEBUG_PRINT_FINAL_STEP:
