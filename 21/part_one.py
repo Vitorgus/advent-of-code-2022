aa2e8e473b4ae497011f35cd8ed85eb9057a0687
Vitor Augusto Pinheiro <26413854+Vitorgus@users.noreply.github.com>
2026-03-06 16:07:06 -0300

    21 part one: solution

---
+++ 21/part_one.py
@@ -0,0 +1,129 @@
+from __future__ import annotations
+
+import os
+import time
+from enum import Enum
+
+start_time = time.time()
+
+# Puzzle inputs and settings
+FILE_NAME = "input.txt"
+DEBUG_PRINT = True
+ROOT_MONKEY = "root"
+
+
+# Classes
+class MathOperator(Enum):
+  PLUS = '+'
+  MINUS = '-'
+  MULTIPLY = '*'
+  DIVIDE = '/'
+
+  @classmethod
+  def from_symbol(cls, symbol: str) -> MathOperator:
+    match symbol:
+      case '/':
+        return MathOperator.DIVIDE
+      case '*':
+        return MathOperator.MULTIPLY
+      case '-':
+        return MathOperator.MINUS
+      case _:
+        return MathOperator.PLUS
+
+  @classmethod
+  def perform_operation(cls, value_1: int, value_2: int, operator: MathOperator) -> int:
+    match operator:
+      case MathOperator.DIVIDE:
+        return value_1 // value_2
+      case MathOperator.MULTIPLY:
+        return value_1 * value_2
+      case MathOperator.MINUS:
+        return value_1 - value_2
+      case _:
+        return value_1 + value_2
+
+class BaseMonkey:
+  def __init__(self, name: str) -> None:
+    self.name = name
+
+  def __str__(self) -> str:
+    return self.name
+
+class NumberMonkey(BaseMonkey):
+  def __init__(self, name: str, value: int) -> None:
+    super().__init__(name)
+    self.value = value
+
+  def __str__(self) -> str:
+    return f'{super().__str__()}: {self.value}'
+
+class MathMonkey(BaseMonkey):
+  def __init__(self, name: str, monkey_1: str, monkey_2: str, operator: MathOperator) -> None:
+    super().__init__(name)
+    self.monkey_1 = monkey_1
+    self.monkey_2 = monkey_2
+    self.operator = operator
+
+  def __str__(self) -> str:
+    return f'{super().__str__()}: {self.monkey_1} {self.operator.value} {self.monkey_2}'
+
+class MonkeyGraph:
+  def __init__(self) -> None:
+    self.monkeys: dict[str, BaseMonkey] = {}
+
+  def add_monkey(self, monkey: BaseMonkey) -> None:
+    self.monkeys[monkey.name] = monkey
+
+  def get_monkey(self, name: str) -> BaseMonkey:
+    return self.monkeys[name]
+
+  def get_monkey_value(self, name: str) -> int:
+    monkey = self.get_monkey(name)
+
+    if isinstance(monkey, NumberMonkey):
+      return monkey.value
+    elif isinstance(monkey, MathMonkey):
+      monkey_1 = self.get_monkey_value(monkey.monkey_1)
+      monkey_2 = self.get_monkey_value(monkey.monkey_2)
+
+      return MathOperator.perform_operation(monkey_1, monkey_2, monkey.operator)
+
+    return -1
+
+
+# Helper funcions
+def get_filepath(file):
+  absolute_path = os.path.dirname(__file__)
+  return os.path.join(absolute_path, file)
+
+
+# Puzzle input parse
+monkey_graph = MonkeyGraph()
+
+with open(get_filepath(FILE_NAME), encoding="utf-8") as f:
+  for line in f:
+    l_strip = line.strip()
+
+    if l_strip != "":
+      data = l_strip.split()
+      name = data[0].replace(":", "")
+
+      monkey = None
+
+      if len(data) == 2:
+        value = int(data[1])
+        monkey = NumberMonkey(name, value)
+      else:
+        monkey_1 = data[1]
+        operator = MathOperator.from_symbol(data[2])
+        monkey_2 = data[3]
+
+        monkey = MathMonkey(name, monkey_1, monkey_2, operator)
+
+      monkey_graph.add_monkey(monkey)
+
+print(monkey_graph.get_monkey_value(ROOT_MONKEY))
+
+print()
+print(f'Execution time: {(time.time() - start_time):.2f}s')
